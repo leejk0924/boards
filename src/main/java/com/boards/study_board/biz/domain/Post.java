1928ca9ea6c50baed9b5576d3e598882e28dfc32
jk <akdirdl@gmail.com>
2023-06-28 00:11:17 +0900

    Fix: 현재시간이 입력되지 않는 에러 수정
    Resolves: #2

--- src/main/java/com/boards/study_board/biz/domain/Post.java
+++ src/main/java/com/boards/study_board/biz/domain/Post.java
@@ -1,16 +1,24 @@
 package com.boards.study_board.biz.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
-    @Id
 /**
  * GenerationType : IDENTITY (PK 자동 증가), SEQUENCE(시퀀스 사용), AUTO(DB 에서 제공하는  PK 사용)
  */
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "POST_ID", length = 20, nullable = false)
     private Long postId;
@@ -27,7 +35,8 @@ public class Post {
     @Column(name = "DELETE_YN", length = 1, nullable = false)
     private Boolean deleteYn;
     @Column(name = "CREATED_DATE", nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
+    // @UpdateTimestamp 어노테이션은 Update 쿼리 발생 시 현재 시간 값을 적용해준다.
+    @UpdateTimestamp
     private LocalDateTime createDate;
     // TimeStamp 속성은 Default null 이 불가능함
     @Column(name = "MODIFIED_DATE")
