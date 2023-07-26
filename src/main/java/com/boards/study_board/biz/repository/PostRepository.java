e4bd147501a82fccd4b8812490a7512cfc5039d5
Jung-Kwon Lee <akdirdl@gmail.com>
2023-07-26 17:18:55 +0900

    remove: 프로젝트 구성 변경으로 인한 파일 삭제

--- src/main/java/com/boards/study_board/biz/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.boards.study_board.biz.repository;
-
-import com.boards.study_board.biz.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
