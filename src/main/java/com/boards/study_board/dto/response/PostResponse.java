a79b252ad2d0ecbac2b8ea843fa04ac19026045a
jk <akdirdl@gmail.com>
2023-07-27 15:04:02 +0900

    remove: 프로젝트 구조 변경으로 인해 불필요한 파일 삭제

--- src/main/java/com/boards/study_board/dto/response/PostResponse.java
+++
@@ -1,6 +0,0 @@
-package com.boards.study_board.dto.response;
-
-import java.time.LocalDate;
-
-public record PostResponse(Long postId, String title, String content, String writer, Integer viewCnt, Boolean noticeYn, Boolean deleteYn, LocalDate createDate, LocalDate modifiedDate) {
-}
